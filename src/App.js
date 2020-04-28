1492a4455b3d8b449eb11a1fbbc2c74fdf610095
ilia <iliailia@me.com>
2020-04-28 17:46:26 +0200

    remove clean up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+
 
 function App() {
   return (
     <div className="App">
-        
+      
     </div>
   );
 }
