b1d2a9c6acfa65471c1b3761b59ee7b57e2cc88a
ilia <iliailia@me.com>
2020-06-02 14:04:56 +0200

    footer section added

--- src/Components/ContentMenu.js
+++ src/Components/ContentMenu.js
@@ -10,10 +10,6 @@ export default function ContentMenu() {
     const currentUser = useRef(user);
     const [profileIcon, setProfileIcon] = useState('');
 
-    const scrollLeft = useSpring({
-        
-    })
-
     useEffect(()=>{
         switch (user){
             case 'tony':
@@ -70,8 +66,8 @@ export default function ContentMenu() {
                     <h1 className="sectionHeader">Movies</h1>
                 </div>
                 <div className="contentSection">
-                    <div id="swipeLeft"></div>
-                    <div id="swipeRight"></div>
+                    {/* <div id="swipeLeft" onClick={()=>{setLeft(!swipeLeft)}}></div>
+                    <div id="swipeRight" onClick={()=>{setRight(!swipeRight)}}></div> */}
                     <div className="col-md-2 selectionWrapper">
                         <div className="overlayText">
 
@@ -130,6 +126,246 @@ export default function ContentMenu() {
                     </div>
                 </div>
             </div>
+
+            <div className="col-md-12 moviesSection">
+                <div className="movieTitleWrapper">
+                    <h1 className="sectionHeader">Series</h1>
+                </div>
+                <div className="contentSection">
+                    {/* <div id="swipeLeft" onClick={()=>{setLeft(!swipeLeft)}}></div>
+                    <div id="swipeRight" onClick={()=>{setRight(!swipeRight)}}></div> */}
+                    <div className="col-md-2 selectionWrapper">
+                        <div className="overlayText">
+
+                        </div>
+                        <div className="selectionBox">
+                            <img src= '/img/movies/tylerRakeExtraction.jpg' alt="tylerRakeMovie"></img>
+                        </div>
+                    </div>
+                    <div className="col-md-2 selectionWrapper">
+                        <div className="overlayText">
+
+                        </div>
+                        <div className="selectionBox">
+                            <img src= '/img/movies/tylerRakeExtraction.jpg' alt="tylerRakeMovie"></img>
+                        </div>
+                    </div>
+                    <div className="col-md-2 selectionWrapper">
+                        <div className="overlayText">
+
+                        </div>
+                        <div className="selectionBox">
+                            <img src= '/img/movies/tylerRakeExtraction.jpg' alt="tylerRakeMovie"></img>
+                        </div>
+                    </div>
+                    <div className="col-md-2 selectionWrapper">
+                        <div className="overlayText">
+
+                        </div>
+                        <div className="selectionBox">
+                            <img src= '/img/movies/tylerRakeExtraction.jpg' alt="tylerRakeMovie"></img>
+                        </div>
+                    </div>
+                    <div className="col-md-2 selectionWrapper">
+                        <div className="overlayText">
+
+                        </div>
+                        <div className="selectionBox">
+                            <img src= '/img/movies/tylerRakeExtraction.jpg' alt="tylerRakeMovie"></img>
+                        </div>
+                    </div>
+                    <div className="col-md-2 selectionWrapper">
+                        <div className="overlayText">
+
+                        </div>
+                        <div className="selectionBox">
+                            <img src= '/img/movies/tylerRakeExtraction.jpg' alt="tylerRakeMovie"></img>
+                        </div>
+                    </div>
+                    <div className="col-md-2 selectionWrapper">
+                        <div className="overlayText">
+
+                        </div>
+                        <div className="selectionBox">
+                            <img src= '/img/movies/tylerRakeExtraction.jpg' alt="tylerRakeMovie"></img>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="col-md-12 moviesSection">
+                <div className="movieTitleWrapper">
+                    <h1 className="sectionHeader">My List</h1>
+                </div>
+                <div className="contentSection">
+                    {/* <div id="swipeLeft" onClick={()=>{setLeft(!swipeLeft)}}></div>
+                    <div id="swipeRight" onClick={()=>{setRight(!swipeRight)}}></div> */}
+                    <div className="col-md-2 selectionWrapper">
+                        <div className="overlayText">
+
+                        </div>
+                        <div className="selectionBox">
+                            <img src= '/img/movies/tylerRakeExtraction.jpg' alt="tylerRakeMovie"></img>
+                        </div>
+                    </div>
+                    <div className="col-md-2 selectionWrapper">
+                        <div className="overlayText">
+
+                        </div>
+                        <div className="selectionBox">
+                            <img src= '/img/movies/tylerRakeExtraction.jpg' alt="tylerRakeMovie"></img>
+                        </div>
+                    </div>
+                    <div className="col-md-2 selectionWrapper">
+                        <div className="overlayText">
+
+                        </div>
+                        <div className="selectionBox">
+                            <img src= '/img/movies/tylerRakeExtraction.jpg' alt="tylerRakeMovie"></img>
+                        </div>
+                    </div>
+                    <div className="col-md-2 selectionWrapper">
+                        <div className="overlayText">
+
+                        </div>
+                        <div className="selectionBox">
+                            <img src= '/img/movies/tylerRakeExtraction.jpg' alt="tylerRakeMovie"></img>
+                        </div>
+                    </div>
+                    <div className="col-md-2 selectionWrapper">
+                        <div className="overlayText">
+
+                        </div>
+                        <div className="selectionBox">
+                            <img src= '/img/movies/tylerRakeExtraction.jpg' alt="tylerRakeMovie"></img>
+                        </div>
+                    </div>
+                    <div className="col-md-2 selectionWrapper">
+                        <div className="overlayText">
+
+                        </div>
+                        <div className="selectionBox">
+                            <img src= '/img/movies/tylerRakeExtraction.jpg' alt="tylerRakeMovie"></img>
+                        </div>
+                    </div>
+                    <div className="col-md-2 selectionWrapper">
+                        <div className="overlayText">
+
+                        </div>
+                        <div className="selectionBox">
+                            <img src= '/img/movies/tylerRakeExtraction.jpg' alt="tylerRakeMovie"></img>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="col-md-12 moviesSection">
+                <div className="movieTitleWrapper">
+                    <h1 className="sectionHeader">Sci-Fi and Fantasy</h1>
+                </div>
+                <div className="contentSection">
+                    {/* <div id="swipeLeft" onClick={()=>{setLeft(!swipeLeft)}}></div>
+                    <div id="swipeRight" onClick={()=>{setRight(!swipeRight)}}></div> */}
+                    <div className="col-md-2 selectionWrapper">
+                        <div className="overlayText">
+
+                        </div>
+                        <div className="selectionBox">
+                            <img src= '/img/movies/tylerRakeExtraction.jpg' alt="tylerRakeMovie"></img>
+                        </div>
+                    </div>
+                    <div className="col-md-2 selectionWrapper">
+                        <div className="overlayText">
+
+                        </div>
+                        <div className="selectionBox">
+                            <img src= '/img/movies/tylerRakeExtraction.jpg' alt="tylerRakeMovie"></img>
+                        </div>
+                    </div>
+                    <div className="col-md-2 selectionWrapper">
+                        <div className="overlayText">
+
+                        </div>
+                        <div className="selectionBox">
+                            <img src= '/img/movies/tylerRakeExtraction.jpg' alt="tylerRakeMovie"></img>
+                        </div>
+                    </div>
+                    <div className="col-md-2 selectionWrapper">
+                        <div className="overlayText">
+
+                        </div>
+                        <div className="selectionBox">
+                            <img src= '/img/movies/tylerRakeExtraction.jpg' alt="tylerRakeMovie"></img>
+                        </div>
+                    </div>
+                    <div className="col-md-2 selectionWrapper">
+                        <div className="overlayText">
+
+                        </div>
+                        <div className="selectionBox">
+                            <img src= '/img/movies/tylerRakeExtraction.jpg' alt="tylerRakeMovie"></img>
+                        </div>
+                    </div>
+                    <div className="col-md-2 selectionWrapper">
+                        <div className="overlayText">
+
+                        </div>
+                        <div className="selectionBox">
+                            <img src= '/img/movies/tylerRakeExtraction.jpg' alt="tylerRakeMovie"></img>
+                        </div>
+                    </div>
+                    <div className="col-md-2 selectionWrapper">
+                        <div className="overlayText">
+
+                        </div>
+                        <div className="selectionBox">
+                            <img src= '/img/movies/tylerRakeExtraction.jpg' alt="tylerRakeMovie"></img>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <footer>
+                    <div className="col-md-12">
+                        <div className="footerWrapperContent">
+                            <div className="footerIcons">
+                                <div className="iconsWrapper">
+                                    <img src='/img/facebookIcon.png' alt="instagram icon"></img>
+                                    <img src='/img/instaIcon.png' alt="instagram icon"></img>
+                                    <img src='/img/twitterIcon.png' alt="twitter icon"></img>
+                                    <img src='/img/youtubeIcon.png' alt="instagram icon"></img>
+                                </div>
+                            </div>
+                            <div className="row footerUpper">
+                                <div className="col-md-3">
+                                        <p>Audio und Untertitel</p>
+                                        <p>Medien-Center</p>
+                                        <p>Datenschutz</p>
+                                        <p>Kontakt</p>
+                                </div>
+                                <div className="col-md-3">
+                                        <p>Audiodiskreption</p>
+                                        <p>Anlegerbeziehungen</p>
+                                        <p>Rechtliche Hinweise</p>
+                                </div>
+                                <div className="col-md-3">
+                                        <p>Hilfe-Center</p>
+                                        <p>Karriere</p>
+                                        <p>Cookie-Einstellungen</p>
+                                </div>
+                                <div className="col-md-3">
+                                        <p>Geschenkkarten</p>
+                                        <p>Nutzungsbedingungen</p>
+                                        <p>Impressum</p>
+                                </div>
+                            </div>
+                            <div className="footerLower">
+                                <Button>Service-Code</Button>
+                                <p>&copy; 1997-2020 Netflix, Inc. i-08932ibuf9bi29f09978882eeee</p>
+                            </div>
+                    </div>
+                </div>
+            </footer>
                 
 
     </div>
